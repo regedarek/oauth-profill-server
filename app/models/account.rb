c48bdbbd474fc9b973390b363af6ed0ee476207a
Darek Finster <darek.finster@gmail.com>
2014-08-23 23:37:40 +0200

    Account has many clients

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,7 @@
 module OauthProfillServer
   module Models
     class Account < ActiveRecord::Base
+      has_many :clients
 
       def as_json(options={})
         super({except: [:password, :created_at]}.merge(options))
