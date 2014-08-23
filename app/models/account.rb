e561f3d5b8ddaf6e14e33490b4ef7cc4a6fd475d
Darek Finster <darek.finster@gmail.com>
2014-08-23 23:37:28 +0200

    Leave updated_at alone

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,12 +1,9 @@
 module OauthProfillServer
   module Models
     class Account < ActiveRecord::Base
-      def updated_at
-        super.strftime("%m/%d/%Y at %I:%M%p")
-      end
 
       def as_json(options={})
-        super({except: [:password, :created_at], methods: :updated_at}.merge(options))
+        super({except: [:password, :created_at]}.merge(options))
       end
     end
   end
