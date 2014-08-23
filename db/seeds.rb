f2560e0c4ffbdfb70bbc75ff44e66789ac1131a3
Darek Finster <darek.finster@gmail.com>
2014-08-23 23:46:08 +0200

    Revert to fresh

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,4 @@
 account = OauthProfillServer::Models::Account.new
 account.username = 'Bartek'
+account.password = 'pass'
 account.save
-
-client = OauthProfillServer::Models::Client.new
-client.identifier    = 'angurailsspike'
-client.account_id    = account.id
-client.secret        = 'application_secret'
-client.name          = 'AnguRailsSpike'
-client.website       = 'http://192.168.0.100:9000'
-client.redirect_uri  = 'http://192.168.0.100:9000/oauth/authorize/callback'
-client.save
