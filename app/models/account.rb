80f9aa64334bfd0cd06c81a21ae5d706c3a4f2ca
Darek Finster <darek.finster@gmail.com>
2014-08-23 18:28:00 +0200

    Update application and account as_json

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,13 @@
 module OauthProfillServer
   module Models
     class Account < ActiveRecord::Base
+      def updated_at
+        super.strftime("%m/%d/%Y at %I:%M%p")
+      end
 
+      def as_json(options={})
+        super({except: [:password, :created_at], methods: :updated_at}.merge(options))
+      end
     end
   end
 end
--- app/models/application.rb
+++ app/models/application.rb
@@ -1,7 +1,9 @@
 module OauthProfillServer
   module Models
     class Application < ActiveRecord::Base
-
+      def as_json(options={})
+        super({except: [:created_at, :updated_at]}.merge(options))
+      end
     end
   end
 end
